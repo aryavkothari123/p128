4b3294a560547a228d56a3f17ac92029df452015
aryavkothari123 <87538366+aryavkothari123@users.noreply.github.com>
2023-04-25 13:46:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+song = "";
+song2 = "";
+leftwristX = 0;
+leftwristY = 0;
+rightwristX = 0;
+rightwristY = 0;
+
+function preload()
+{
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 350);
+    canvas.position(475, 300);
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+	stroke("#FF0000");
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+	song.rate(1);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		console.log(results);
+		leftwristX = results[0].pose.leftWrist.x;
+		leftwristY = results[0].pose.leftWrist.y;
+		console.log("LeftwristX = " + leftwristX + "LeftwristY = " + leftwristY);
+
+		rightwristX = results[0].pose.rightWrist.x;
+		rightwristY = results[0].pose.rightWrist.y;
+		console.log("RightwristX = " + rightwristX + "RightwristY = " + rightwristY);
+	}
+}
